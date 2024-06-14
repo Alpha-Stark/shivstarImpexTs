044d3fa9052c9d23c47bc9cde3abf24dbd2b9a78
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-06-14 17:26:06 +0530

    testing version

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
-// import { connectToDatabase } from "@/lib/database";
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { connectToDatabase } from "@/lib/database";
 
 
 export async function POST(req: Request) {
 
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+    connectToDatabase();
 
     if (!WEBHOOK_SECRET) {
         throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
