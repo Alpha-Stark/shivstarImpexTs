d6d4434adf348a092b76773f9e224586476f3644
MANN SAVANI <96766945+AlphaSmashers@users.noreply.github.com>
2024-06-29 12:18:31 +0530

    Mobile Responsive :About section

--- Components/Abouts.tsx
+++ Components/Abouts.tsx
@@ -8,9 +8,9 @@ const Abouts = () => {
                 <div className={styles.HomePage_firstDiv}>
                     <div className={styles.HomePage_firstDiv_child}>
                         <h1>About</h1>
-                        <p className={`${styles.text_black} text-black text-opacity-70 font-sans text-base font-medium mb-8 w-3/4`}>Shivstar Impex is More Than Just Gliding</p>
-                        <p className="text-black text-opacity-70 font-sans text-base font-medium mb-8 w-3/4">Our Company is an exclusive supplier of Diamonds with the world's best quality. We take pride in offering our customers only the highest quality products.</p>
-                        <p className="text-black text-opacity-70 font-sans text-base font-medium mb-8 w-3/4">We are constantly expanding our range to meet the needs of our customers and offer them the latest and most fashionable trends in Diamonds. We are confident that our collection of Diamonds will allow everyone to express their individual style and create a unique image</p>
+                        <p className={`${styles.text_black} text-black text-opacity-70 font-sans text-base font-medium mb-8`}>Shivstar Impex is More Than Just Gliding</p>
+                        <p className="text-black text-opacity-70 font-sans text-base font-medium mb-8">Our Company is an exclusive supplier of Diamonds with the world's best quality. We take pride in offering our customers only the highest quality products.</p>
+                        <p className="text-black text-opacity-70 font-sans text-base font-medium mb-8">We are constantly expanding our range to meet the needs of our customers and offer them the latest and most fashionable trends in Diamonds. We are confident that our collection of Diamonds will allow everyone to express their individual style and create a unique image</p>
                         <span>OUR STORES</span>
                     </div>
                     <div className={styles.HomePage_frstDiv2}>
